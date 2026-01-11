c2e90f2517242c828d93b60b22e71f8fde7b3060
Nicholas <135126451+djuntu@users.noreply.github.com>
2026-01-11 02:03:06 +0000

    Create Writer.lua

---
+++ lib/Create/Gui/Components/Writer.lua
@@ -0,0 +1,73 @@
+local Types = require(script.Parent.Parent.Types)
+
+local Writer = {}
+Writer.__index = Writer
+
+local HEADER_TEXT_COLOR_3 = Color3.fromRGB(26, 255, 0)
+
+function Writer.new ()
+	local self = {}
+	self.Object = nil
+
+	setmetatable(self, Writer)
+	return self
+end
+
+function Writer:SetContent (text: string)
+	self.Object:FindFirstChild('TextBox').Text = text
+end
+
+function Writer:SetContentColor(color: Color3)
+    self.Object:FindFirstChild('TextBox').TextColor3 = color
+end
+
+function Writer:SetHeader (text: string)
+	self.Object:FindFirstChild('TextLabel').Text = text
+end
+
+function Writer:Create (parent: GuiObject)
+	local Frame = Instance.new('Frame')
+	Frame.Name = 'Writer'
+	Frame.BackgroundTransparency = 1
+	Frame.Size = UDim2.new(UDim.new(1, 0), UDim.new(0, 20))
+
+	local Content = Instance.new('TextBox')
+	Content.TextEditable = true
+	Content.Text = ''
+	Content.PlaceholderText = ''
+	Content.PlaceholderColor3 = Color3.new(1, 1, 1)
+	Content.BackgroundTransparency = 1
+	Content.ClearTextOnFocus = false
+	Content.Interactable = true
+	Content.Size = UDim2.new(UDim.new(1, 0), UDim.new(0, 20))
+	Content.FontFace = Font.fromEnum(Enum.Font.RobotoMono)
+	Content.TextColor3 = Color3.new(1, 1, 1)
+	Content.TextWrapped = true
+	Content.TextXAlignment = Enum.TextXAlignment.Left
+	Content.TextSize = 18
+	Content.LayoutOrder = 1
+	Content.RichText = true
+	Content.Parent = Frame
+
+	local User = Instance.new('TextLabel')
+	User.BackgroundTransparency = 1
+	User.FontFace = Font.fromEnum(Enum.Font.RobotoMono)
+	User.TextColor3 = HEADER_TEXT_COLOR_3
+	User.TextXAlignment = Enum.TextXAlignment.Left
+	User.TextSize = 18
+	User.Size = UDim2.fromOffset(0, 20)
+	User.AutomaticSize = Enum.AutomaticSize.X
+	User.Parent = Frame
+
+    local Layout = Instance.new('UIListLayout')
+    Layout.FillDirection = Enum.FillDirection.Horizontal
+    Layout.Padding = UDim.new(0, 5)
+    Layout.SortOrder = Enum.SortOrder.LayoutOrder
+    Layout.Parent = Frame
+
+    Frame.Parent = parent
+	self.Object = Frame
+	return self
+end
+
+return Writer
