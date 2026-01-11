06ea72d34404c3ee2622b9d97922e6a12d7c638a
Nicholas <135126451+djuntu@users.noreply.github.com>
2026-01-11 02:03:08 +0000

    Create Events.lua

---
+++ lib/Create/Gui/Events.lua
@@ -0,0 +1,13 @@
+local Types = require(script.Parent.Types)
+local Events: Types.Events = {
+    Main = nil
+}
+
+Events.OnTextChange = function()
+    Events.Main.Window:GoToFocus()
+end
+
+return function (Main)
+    Events.Main = Main
+    return Events
+end
